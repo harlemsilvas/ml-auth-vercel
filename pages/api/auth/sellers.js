abcd240723c97b8d3a57df556d086ab00e977b32
harlemsilvas <harlem.claumann@gmail.com>
2025-09-11 14:00:17 -0300

    auth\sellers.js

--- pages/api/auth/sellers.js
+++ pages/api/auth/sellers.js
@@ -1,4 +1,5 @@
 // pages/api/auth/sellers.js
+// 11-9-2025 correção de erro na vercel
 import { getAllSellers } from "../../../services/token-manager";
 
 export default async function handler(req, res) {
